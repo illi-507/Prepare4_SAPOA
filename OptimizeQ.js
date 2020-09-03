9d38856d0c2188888c3963c0ebbe907239bfb7d8
XINGJIAN WANG <wangxingjian94@gmail.com>
2020-09-03 02:08:45 -0400

    refine q5 q7

---
+++ OptimizeQ.js
@@ -0,0 +1,11 @@
+console.log("WDNMD");
+
+console.log("use alt attribute with image tag");
+console.log("Use meaningful names for your Ids and Classes. They should be short, descriptive and represent only one concept. It’ll make your HTML clearer and the styling process easier.")
+
+console.log("Use whitespace: indentation, empty lines, line breaks. Other humans should be able to read your code in the future without having headache. ");
+
+console.log("The “lang” attribute One reason for sticking with the use of optional tags, such as the <html> tag, is the use of attributes. It is considered best practice for internationalization purposes, according to W3C, to always declare the default text language of a page in the <html> tag.");
+
+console.log("declare the title tag, metatag and character encoding in head");
+console.log("<section> is a semantic markup tag, not a stylistic one It should not be used to tag a wrapper, a container or any other purely stylistic block. So, for example, this is not a correct use of the <section> tag:");
